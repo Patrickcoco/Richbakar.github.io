cbc6d73c52a7493005ae2712180127b7bc3f77d5
Patrick Julian Rusli <patrickjulianrusli@gmail.com>
2025-10-01 05:59:10 +0700

    update gambar menu dah animasi

--- menu.js
+++ menu.js
@@ -16,3 +16,31 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+// Navbar Scroll Effect
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+// Animasi scroll (reveal)
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  reveals.forEach((el) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = el.getBoundingClientRect().top;
+    const revealPoint = 100; // jarak sebelum muncul
+
+    if (elementTop < windowHeight - revealPoint) {
+      el.classList.add('active');
+    } else {
+      el.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
